0be2276ff3b7e3040eaaa561e0e4376d3230eb99
Anton <webmakiam@gmail.com>
2019-12-14 05:28:40 +0100

    Form commentaire

--- view/fullArticle.php
+++ view/fullArticle.php
@@ -1,17 +1,24 @@
-<?php ob_start(); $title = "News";?>
+<?php ob_start();
+$title = "News"; ?>
 
+<!-- Affichage article entier -->
 <div id="article">
-<img class="picture2" src="public/img/<?= $myArticle->getImg() ?>">
-<p> <?= $myArticle->getTitle() ?> </p>
-<p> <?= $myArticle->getContent() ?> </p>
-<span class="dateAuthor">Posté le <?= $myArticle->getDate() ?> par : <?= $myArticle->getAuthor() ?></span>
+        <img class="picture2" src="public/img/<?= $myArticle->getImg() ?>">
+        <p> <?= $myArticle->getTitle() ?> </p>
+        <p> <?= $myArticle->getContent() ?> </p>
+        <span class="dateAuthor">Posté le <?= $myArticle->getDate() ?> par : <?= $myArticle->getAuthor() ?></span>
 </div>
 <hr>
+<form action="#" method="POST">
+        <textarea rows="3" cols="100" name="newComment">Votre commentaire</textarea>
+</form>
 <h3>Commentaires de l'article</h3>
-<?php foreach ($myComments as $comment) { ?>          
+
+<?php foreach ($myComments as $comment) { ?>
         <p><?= $comment->getComment() ?></p>
-        <br><hr>    
-<?php } 
-$content= ob_get_clean();
+        <br>
+        <hr>
+<?php }
+$content = ob_get_clean();
 require 'template.php';
 ?>
